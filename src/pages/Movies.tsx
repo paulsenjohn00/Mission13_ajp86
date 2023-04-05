23443d0d3e4536c990cf99156ffdac04d801a019
ajp86 <paulsenjohn00@gmail.com>
2023-04-05 09:03:23 -0600

    mission14 changes

--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -1,8 +1,19 @@
-import data from '../MovieData.json';
-
-const Movies = data.MovieData;
+import { useEffect, useState } from 'react';
+import { Movie } from '../types/movie';
 
 function MovieList() {
+  const [movieData, setMovieData] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const rsp = await fetch('http://localhost:4000/movies');
+      const temp = await rsp.json();
+      setMovieData(temp);
+    };
+
+    fetchMovies();
+  }, []);
+
   return (
     <>
       <div className="text-center p-1 m-1">
@@ -11,23 +22,27 @@ function MovieList() {
       <table className="table m-2 p-2">
         <thead>
           <tr>
+            <th>Category</th>
             <th>Title</th>
             <th>Year</th>
             <th>Director</th>
             <th>Rating</th>
-            <th>Category</th>
             <th>Edited</th>
+            <th>Lent To</th>
+            <th>Notes</th>
           </tr>
         </thead>
         <tbody>
-          {Movies.map((m) => (
-            <tr>
-              <td>{m.Title}</td>
-              <td>{m.Year}</td>
-              <td>{m.Director}</td>
-              <td>{m.Rating}</td>
-              <td>{m.Category}</td>
-              <td>{m.Edited}</td>
+          {movieData.map((m) => (
+            <tr key={m.movieId}>
+              <td>{m.category}</td>
+              <td>{m.title}</td>
+              <td>{m.year}</td>
+              <td>{m.director}</td>
+              <td>{m.rating}</td>
+              <td>{m.edited}</td>
+              <td>{m.lentTo}</td>
+              <td>{m.notes}</td>
             </tr>
           ))}
         </tbody>
